48ebb48b46ca58758d8aa8ba220adaffe8c4df72
Michael Bruhn <mib@michaelbruhn.com>
2024-10-21 07:47:39 +0200

    Update setup.sql

--- Empl_Model/setup.sql
+++ Empl_Model/setup.sql
@@ -1,19 +1,5 @@
  
- /*Create tables*/
- /* drop table departments;
-  drop table departments_row;
-  drop TABLE dept_emp;
-  drop TABLE dept_emp_row;
-  drop TABLE dept_manager;
-  drop TABLE dept_manager_row;
-  drop table employees;
-  drop table employees_row;
-  drop table salaries;
-  drop table salaries_row;
-  drop table titles;
-  drop table titles_row;
-  */
-  
+ /*STEP 1: Create tables*/
   
   CREATE COLUMN TABLE departments 
   (  dept_no NVARCHAR(4) primary key,
@@ -125,11 +111,25 @@
 	primary key (emp_no, title, from_date)
    );  
    
+
+
+
+
+
+
+
    
-   
-   
-/*Load data into _ROW tables first - 
-  Then execute the following lines */
+/*STEP 3: copy row table contents into col based tables*/
+/*  INSERT INTO departments SELECT * FROM departments_row;
+  INSERT INTO dept_emp SELECT * FROM dept_emp_row;
+  INSERT INTO dept_manager SELECT * FROM dept_manager_row;
+  INSERT INTO employees SELECT * FROM employees_row;
+  INSERT INTO salaries SELECT * FROM salaries_row;
+  INSERT INTO titles SELECT * FROM titles_row;
+*/
+  
+  
+/*STEP 4: copy row table contents into col based tables*/
 /*  select 'Departments' as tablename, count(*) from departments union all
   select 'Departments_row' as tablename, count(*) from departments_row union all 
   select 'dept_emp' as tablename, count(*) from dept_emp union all
@@ -142,24 +142,27 @@
   select 'salaries_row' as tablename, count(*) from salaries_row union all
   select 'titles' as tablename, count(*) from  titles union all
   select 'titles_row' as tablename, count(*) from titles_row ;
-*/
+*/  
 
-/*Replicate table from row tables into column based*/
-/*  INSERT INTO departments SELECT * FROM departments_row;
-  INSERT INTO dept_emp SELECT * FROM dept_emp_row;
-  INSERT INTO dept_manager SELECT * FROM dept_manager_row;
-  INSERT INTO employees SELECT * FROM employees_row;
-  INSERT INTO salaries SELECT * FROM salaries_row;
-  INSERT INTO titles SELECT * FROM titles_row;
-  */
-  
-  
-  
-  
-  -- CREATE ROLE WORKSHOP_TESTDATA
-  -- GRANT select on  to _SYS_REPO with grant option;
 
-  /*Grant access to tables and schemas to _sys_repo
-     - and other users who whould be able to read them 
+/*STEP 5: grant schema access to _SYS_REPO*/
+ /* 
+  GRANT select ON SCHEMA to _SYS_REPO with grant option;
+ */
+ 
 
+/*STEP 6: Drop created tables when the workshop is done */
+ /* 
+  drop table departments;
+  drop table departments_row;
+  drop table dept_emp;
+  drop table dept_emp_row;
+  drop table dept_manager;
+  drop table dept_manager_row;
+  drop table employees;
+  drop table employees_row;
+  drop table salaries;
+  drop table salaries_row;
+  drop table titles;
+  drop table titles_row;
   */
